67f46ce9a6f9294aecc928ceb56951978be550a2
dbr <dbr.onix@gmail.com>
2020-08-28 20:43:25 +0930

    Refactor into library with separate CLI

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 use std::str::FromStr;
 
+use serde_derive::{Deserialize, Serialize};
+
 pub struct VideoInfo {
+    pub id: String,
     pub url: String,
     pub title: String,
+    pub thumbnail: String,
 }
 
 pub struct YtUploadsCrawler {
@@ -73,8 +77,11 @@ impl YtUploadsCrawler {
     fn start(&mut self) {
         match self.uploads_page_data() {
             Ok(data) => {
-                let section_list_renderer = &data[1]["response"]["contents"]["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]["sectionListRenderer"];
-                let item_section_renderer = &section_list_renderer["contents"][0]["itemSectionRenderer"];
+                let section_list_renderer = &data[1]["response"]["contents"]
+                    ["twoColumnBrowseResultsRenderer"]["tabs"][1]["tabRenderer"]["content"]
+                    ["sectionListRenderer"];
+                let item_section_renderer =
+                    &section_list_renderer["contents"][0]["itemSectionRenderer"];
                 let grid_renderer = &item_section_renderer["contents"][0]["gridRenderer"];
 
                 let items = &grid_renderer["items"];
@@ -87,10 +94,10 @@ impl YtUploadsCrawler {
 
                 self.collect_links(&items);
                 self.next_continuation = next_continuation.clone();
-            },
+            }
             Err(error) => {
                 self.error = Some(FetchLinksError::from(error));
-            },
+            }
         }
     }
 
@@ -104,14 +111,15 @@ impl YtUploadsCrawler {
                 let grid_continuation = &continuation_contents["gridContinuation"];
 
                 let items = &grid_continuation["items"];
-                let next_continuation = &grid_continuation["continuations"][0]["nextContinuationData"];
+                let next_continuation =
+                    &grid_continuation["continuations"][0]["nextContinuationData"];
 
                 self.collect_links(&items);
                 self.next_continuation = next_continuation.clone();
-            },
+            }
             Err(error) => {
                 self.error = Some(FetchLinksError::from(error));
-            },
+            }
         }
     }
 
@@ -122,11 +130,21 @@ impl YtUploadsCrawler {
         for item in items.as_array().unwrap().iter() {
             // dbg!(item);
             let video_id = item["gridVideoRenderer"]["videoId"].as_str().unwrap();
-            let info = VideoInfo{
+            let info = VideoInfo {
+                id: video_id.into(),
                 url: make_youtube_video_url(video_id),
-                title: item["gridVideoRenderer"]["title"]["runs"][0]["text"].as_str().unwrap_or(
-                    item["gridVideoRenderer"]["title"]["simpleText"].as_str().unwrap_or(""),
-                ).into(),
+                title: item["gridVideoRenderer"]["title"]["runs"][0]["text"]
+                    .as_str()
+                    .unwrap_or(
+                        item["gridVideoRenderer"]["title"]["simpleText"]
+                            .as_str()
+                            .unwrap_or(""),
+                    )
+                    .into(),
+                thumbnail: item["gridVideoRenderer"]["thumbnail"]["thumbnails"][0]["url"]
+                    .as_str()
+                    .unwrap_or("")
+                    .into(),
             };
             self.links.push(info);
         }
@@ -141,12 +159,15 @@ impl YtUploadsCrawler {
         for worker in candidates.iter() {
             let request = worker(&self.channel);
 
-            let mut response = self.tokio_runtime.block_on(self.http_client.request(request)).unwrap();
+            let mut response = self
+                .tokio_runtime
+                .block_on(self.http_client.request(request))
+                .unwrap();
 
             if let hyper::StatusCode::OK = response.status() {
                 let data = hyper_response_body_as_string(&mut self.tokio_runtime, &mut response)?;
 
-                return Ok(parse_json_data(&data)?)
+                return Ok(parse_json_data(&data)?);
             }
         }
 
@@ -156,14 +177,20 @@ impl YtUploadsCrawler {
     fn uploads_continuation_data(&mut self) -> Result<serde_json::Value, RequestError> {
         let request = request_browse(
             self.next_continuation["continuation"].as_str().unwrap(),
-            self.next_continuation["clickTrackingParams"].as_str().unwrap());
+            self.next_continuation["clickTrackingParams"]
+                .as_str()
+                .unwrap(),
+        );
 
-        let mut response = self.tokio_runtime.block_on(self.http_client.request(request)).unwrap();
+        let mut response = self
+            .tokio_runtime
+            .block_on(self.http_client.request(request))
+            .unwrap();
 
         if let hyper::StatusCode::OK = response.status() {
             let data = hyper_response_body_as_string(&mut self.tokio_runtime, &mut response)?;
 
-            return Ok(parse_json_data(&data)?)
+            return Ok(parse_json_data(&data)?);
         } else {
             Err(RequestError::NotFound)
         }
@@ -174,8 +201,9 @@ fn request_channel_uploads(channel: &str) -> hyper::Request<hyper::Body> {
     let url = hyper::Uri::from_str(
         &(String::from("https://www.youtube.com/channel/")
             + channel
-            + "/videos?view=0&flow=grid&pbj=1"))
-        .unwrap();
+            + "/videos?view=0&flow=grid&pbj=1"),
+    )
+    .unwrap();
 
     let request = hyper::Request::builder()
         .method("GET")
@@ -190,10 +218,9 @@ fn request_channel_uploads(channel: &str) -> hyper::Request<hyper::Body> {
 
 fn request_user_uploads(user: &str) -> hyper::Request<hyper::Body> {
     let url = hyper::Uri::from_str(
-        &(String::from("https://www.youtube.com/user/")
-            + user
-            + "/videos?view=0&flow=grid&pbj=1"))
-        .unwrap();
+        &(String::from("https://www.youtube.com/user/") + user + "/videos?view=0&flow=grid&pbj=1"),
+    )
+    .unwrap();
 
     let request = hyper::Request::builder()
         .method("GET")
@@ -219,8 +246,9 @@ fn request_browse(ctoken: &str, itct: &str) -> hyper::Request<hyper::Body> {
     };
     let url = hyper::Uri::from_str(
         &(String::from("https://www.youtube.com/browse_ajax?")
-            + &serde_qs::to_string(&query).unwrap()))
-        .unwrap();
+            + &serde_qs::to_string(&query).unwrap()),
+    )
+    .unwrap();
 
     let request = hyper::Request::builder()
         .method("GET")
@@ -234,9 +262,12 @@ fn request_browse(ctoken: &str, itct: &str) -> hyper::Request<hyper::Body> {
 }
 
 fn hyper_response_body_as_string(
-        tokio_runtime: &mut tokio::runtime::Runtime,
-        response: &mut hyper::Response<hyper::Body>) -> Result<String, hyper::Error> {
-    let bytes = tokio_runtime.block_on(hyper::body::to_bytes(response.body_mut())).unwrap();
+    tokio_runtime: &mut tokio::runtime::Runtime,
+    response: &mut hyper::Response<hyper::Body>,
+) -> Result<String, hyper::Error> {
+    let bytes = tokio_runtime
+        .block_on(hyper::body::to_bytes(response.body_mut()))
+        .unwrap();
 
     Ok(String::from_utf8(bytes.to_vec()).unwrap())
 }
@@ -305,7 +336,9 @@ impl From<std::io::Error> for RequestError {
 #[derive(Debug)]
 pub enum ParseJsonDataError {
     Io(std::io::Error),
+    /// JSON parse error
     Parse(serde_json::Error),
+    /// Unexpected HTML response from URL which should respond with JSON data
     Html(String),
 }
 
